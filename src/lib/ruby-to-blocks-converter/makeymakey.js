5df41fa3d5d0aba78f1f54b38a2d91a9a9500a0c
kijiharu <kijiharu3112@gmail.com>
2022-06-30 17:22:03 +0900

    Makeymakeynのrubyからブロックへの変換完成

---
+++ src/lib/ruby-to-blocks-converter/makeymakey.js
@@ -0,0 +1,46 @@
+/* global Opal */
+import _ from 'lodash';
+
+/**
+ * MakeyMakey converter
+ */
+const MakeyMakeyConverter = {
+    // eslint-disable-next-line no-unused-vars
+    onSend: function (receiver, name, args, rubyBlockArgs, rubyBlock, node) {
+        let block;
+        if ((this._isSelf(receiver) || receiver === Opal.nil) &&
+            name === 'when' &&
+            args.length === 2 && args[0].type === 'sym' &&
+            rubyBlockArgs && rubyBlockArgs.length === 0 &&
+            rubyBlock) {
+            switch (args[0].value) {
+            case 'makey_key_pressed':
+                console.log('if');
+                if (this._isStringOrBlock(args[1])) {
+                    block = this._createBlock('makeymakey_whenMakeyKeyPressed', 'hat');
+                    this._addInput(
+                        block,
+                        'KEY',
+                        this._createFieldBlock('makeymakey_menu_KEY', 'KEY', args[1]),
+                    );
+                    this._setParent(rubyBlock, block);
+                }
+                break;
+            case 'makey_pressed_in_oder':
+                if (this._isStringOrBlock(args[1])) {
+                    block = this._createBlock('makeymakey_whenCodePressed', 'hat');
+                    this._addInput(
+                        block, 
+                        'SEQUENCE',
+                        this._createFieldBlock('makeymakey_menu_SEQUENC', 'SEQUENCE', args[1])
+                    );
+                    this._setParent(rubyBlock, block);
+                }
+                break;
+            }
+        }
+        return block;
+    }
+};
+
+export default MakeyMakeyConverter;
