9ea2de18f93c96b88baa8410e1f11dc73372543c
Ko-ichiro Sugiyama <sugiyama@gfd-dennou.org>
2023-11-06 18:53:56 +0900

    Change Icons (extensions)

--- src/lib/libraries/extensions/index.jsx
+++ src/lib/libraries/extensions/index.jsx
@@ -1,110 +1,14 @@
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
 
-import sampleIconURL from './smalrubot-s1/smalrubot-s1.png';
-import sampleInsetIconURL from  './smalrubot-s1/smalrubot-s1-small.png'
-;
+import menu1IconURL from './smt/kani.png';
+import menu1InsetIconURL from  './smt/kani-small.png';
+import menu2IconURL from './smt/mboard.png';
+import menu2InsetIconURL from  './smt/mboard-small.png';
+import menu3IconURL from './smt/microcom.png';
+import menu3InsetIconURL from  './smt/microcom-small.png';
+
 const extensions = [
-    {
-        name: (
-            <FormattedMessage
-                defaultMessage="Mboard1"
-                description="Name for the 'Mboard1' extension"
-                id="gui.mboard1.name"
-            />
-        ),
-        extensionId: 'mboard1',
-        iconURL: sampleIconURL,
-        insetIconURL: sampleInsetIconURL,
-        description: (
-            <FormattedMessage
-                defaultMessage="Matsue Board Blocks (Easy)"
-                description="Description for the 'mboard1' extension"
-                id="gui.mboard1.description"
-            />
-        ),
-        featured: true
-    },
-    {
-        name: (
-            <FormattedMessage
-                defaultMessage="Mboard2"
-                description="Name for the 'Mboard2' extension"
-                id="gui.mboard2.name"
-            />
-        ),
-        extensionId: 'mboard2',
-        iconURL: sampleIconURL,
-        insetIconURL: sampleInsetIconURL,
-        description: (
-            <FormattedMessage
-                defaultMessage="Matsue Board Blocks (Normal)"
-                description="Description for the 'mboard2' extension"
-                id="gui.mboard2.description"
-            />
-        ),
-        featured: true
-    },
-    {
-        name: (
-            <FormattedMessage
-                defaultMessage="Mboard3"
-                description="Name for the 'Mboard3' extension"
-                id="gui.mboard3.name"
-            />
-        ),
-        extensionId: 'mboard3',
-        iconURL: sampleIconURL,
-        insetIconURL: sampleInsetIconURL,
-        description: (
-            <FormattedMessage
-                defaultMessage="Matsue Board Blocks"
-                description="Description for the 'mboard3' extension"
-                id="gui.mboard3.description"
-            />
-        ),
-        featured: true
-    },
-    {
-        name: (
-            <FormattedMessage
-                defaultMessage="Microcom"
-                description="Name for the 'Micro_Computer' extension"
-                id="gui.microcom.name"
-            />
-        ),
-        extensionId: 'microcom',
-        iconURL: sampleIconURL,
-        insetIconURL: sampleInsetIconURL,
-        description: (
-            <FormattedMessage
-                defaultMessage="Micro Computer Blocks"
-                description="Description for the 'Microcom' extension"
-                id="gui.microcom.description"
-            />
-        ),
-        featured: true
-    },
-    {
-        name: (
-            <FormattedMessage
-                defaultMessage="M5stack"
-                description="Name for the 'M5stack' extension"
-                id="gui.m5stack.name"
-            />
-        ),
-        extensionId: 'm5stack',
-        iconURL: sampleIconURL,
-        insetIconURL: sampleInsetIconURL,
-        description: (
-            <FormattedMessage
-                defaultMessage="M5stack Blocks"
-                description="Description for the 'M5stack' extension"
-                id="gui.m5stack.description"
-            />
-        ),
-        featured: true
-    },
     {
         name: (
             <FormattedMessage
@@ -114,8 +18,8 @@ const extensions = [
             />
         ),
         extensionId: 'kanirobo1',
-        iconURL: sampleIconURL,
-        insetIconURL: sampleInsetIconURL,
+        iconURL: menu1IconURL,
+        insetIconURL: menu1InsetIconURL,
         description: (
             <FormattedMessage
                 defaultMessage="Kanirobo Blocks (Easy)"
@@ -134,33 +38,13 @@ const extensions = [
             />
         ),
         extensionId: 'kanirobo2',
-        iconURL: sampleIconURL,
-        insetIconURL: sampleInsetIconURL,
+        iconURL: menu1IconURL,
+        insetIconURL: menu1InsetIconURL,
         description: (
             <FormattedMessage
                 defaultMessage="Kanirobo Blocks (Normal)"
                 description="Description for the 'Kanirobo2' extension"
                 id="gui.kanirobo2.description"
-            />
-        ),
-        featured: true
-    },
-    {
-        name: (
-            <FormattedMessage
-                defaultMessage="Sensor"
-                description="Name for the 'Sensor' extension"
-                id="gui.sensor.name"
-            />
-        ),
-        extensionId: 'sensor',
-        iconURL: sampleIconURL,
-        insetIconURL: sampleInsetIconURL,
-        description: (
-            <FormattedMessage
-                defaultMessage="Sensor (peripherals)"
-                description="Description for the 'sensor' extension"
-                id="gui.sensor.description"
             />
         ),
         featured: true
@@ -174,8 +58,8 @@ const extensions = [
             />
         ),
         extensionId: 'tools',
-        iconURL: sampleIconURL,
-        insetIconURL: sampleInsetIconURL,
+        iconURL: menu1IconURL,
+        insetIconURL: menu1InsetIconURL,
         description: (
             <FormattedMessage
                 defaultMessage="Tools (output, etc.)"
@@ -184,7 +68,127 @@ const extensions = [
             />
         ),
         featured: true
-    }
+    },
+    {
+        name: (
+            <FormattedMessage
+                defaultMessage="Mboard1"
+                description="Name for the 'Mboard1' extension"
+                id="gui.mboard1.name"
+            />
+        ),
+        extensionId: 'mboard1',
+        iconURL: menu2IconURL,
+        insetIconURL: menu2InsetIconURL,
+        description: (
+            <FormattedMessage
+                defaultMessage="Matsue Board Blocks (Easy)"
+                description="Description for the 'mboard1' extension"
+                id="gui.mboard1.description"
+            />
+        ),
+        featured: true
+    },
+    {
+        name: (
+            <FormattedMessage
+                defaultMessage="Mboard2"
+                description="Name for the 'Mboard2' extension"
+                id="gui.mboard2.name"
+            />
+        ),
+        extensionId: 'mboard2',
+        iconURL: menu2IconURL,
+        insetIconURL: menu2InsetIconURL,
+        description: (
+            <FormattedMessage
+                defaultMessage="Matsue Board Blocks (Normal)"
+                description="Description for the 'mboard2' extension"
+                id="gui.mboard2.description"
+            />
+        ),
+        featured: true
+    },
+    {
+        name: (
+            <FormattedMessage
+                defaultMessage="Mboard3"
+                description="Name for the 'Mboard3' extension"
+                id="gui.mboard3.name"
+            />
+        ),
+        extensionId: 'mboard3',
+        iconURL: menu2IconURL,
+        insetIconURL: menu2InsetIconURL,
+        description: (
+            <FormattedMessage
+                defaultMessage="Matsue Board Blocks"
+                description="Description for the 'mboard3' extension"
+                id="gui.mboard3.description"
+            />
+        ),
+        featured: true
+    },
+    {
+        name: (
+            <FormattedMessage
+                defaultMessage="Microcom"
+                description="Name for the 'Micro_Computer' extension"
+                id="gui.microcom.name"
+            />
+        ),
+        extensionId: 'microcom',
+        iconURL: menu3IconURL,
+        insetIconURL: menu3InsetIconURL,
+        description: (
+            <FormattedMessage
+                defaultMessage="Micro Computer Blocks"
+                description="Description for the 'Microcom' extension"
+                id="gui.microcom.description"
+            />
+        ),
+        featured: true
+    },
+    {
+        name: (
+            <FormattedMessage
+                defaultMessage="M5stack"
+                description="Name for the 'M5stack' extension"
+                id="gui.m5stack.name"
+            />
+        ),
+        extensionId: 'm5stack',
+        iconURL: menu3IconURL,
+        insetIconURL: menu3InsetIconURL,
+        description: (
+            <FormattedMessage
+                defaultMessage="M5stack Blocks"
+                description="Description for the 'M5stack' extension"
+                id="gui.m5stack.description"
+            />
+        ),
+        featured: true
+    },
+    {
+        name: (
+            <FormattedMessage
+                defaultMessage="Sensor"
+                description="Name for the 'Sensor' extension"
+                id="gui.sensor.name"
+            />
+        ),
+        extensionId: 'sensor',
+        iconURL: menu3IconURL,
+        insetIconURL: menu3InsetIconURL,
+        description: (
+            <FormattedMessage
+                defaultMessage="Sensor (peripherals)"
+                description="Description for the 'sensor' extension"
+                id="gui.sensor.description"
+            />
+        ),
+        featured: true
+    },
 ];
 
 export default extensions;
