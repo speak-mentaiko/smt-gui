6b6acff48024acafe8663f3734c281e35b2b108e
Ko-ichiro Sugiyama <sugiyama@gfd-dennou.org>
2023-11-10 21:48:12 +0900

    servo motor block is modified

--- src/lib/ruby-generator/kanirobo1.js
+++ src/lib/ruby-generator/kanirobo1.js
@@ -24,10 +24,14 @@ export default function (Generator) {
     Generator.kanirobo1_servo_init = function (block) {
 	return `servo27 = PWM.new(27, ch=3)\n` +
 	    `servo14 = PWM.new(14, ch=2)\n` +
-	    `servo27.freq( 50 )\n` +
-	    `servo14.freq( 50 )\n` +
-            `servo27.duty( 0 )\n` + 	    
-            `servo14.duty( 0 )\n` ;
+	    `servo27.freq( 80 )\n` +
+	    `servo14.freq( 80 )\n` +
+	    `deg = ((( 0 - 90.0) * 0.95 / 90.0 + 1.45) / 20.0 * 1024).to_i\n` +
+            `servo27.duty( deg )\n` +
+            `servo14.duty( deg )\n` +
+	    `sleep 0.1\n` +
+            `servo27.deinit\n` +
+            `servo14.deinit\n` ;
     };
     
     Generator.kanirobo1_motor = function (block) {
@@ -50,9 +54,10 @@ export default function (Generator) {
         const text = Generator.valueToCode(block, 'TEXT', Generator.ORDER_NONE) || null;
         const num  = Generator.valueToCode(block, 'NUM', Generator.ORDER_NONE)  || 0;
 	return `deg = (((${num}.to_f - 90.0) * 0.95 / 90.0 + 1.45) / 20.0 * 1024).to_i\n`+
-               `servo${text}.duty( ( deg % 1024 ).to_i )\n`;
+               `servo${text}.duty( ( deg % 1024 ).to_i )\n` +
+	       `sleep 0.1\n` +
+	       `servo${text}.deinit\n`;
     };
-
 
     // メニューについては Ruby 側でも定義が必要のようだ
     Generator.kanirobo1_menu_menu1 = function (block){
