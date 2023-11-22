d59577e40986feed53c0468b4bd8865c07216dad
Tatsuto YAMAMOTO <me@laminne33569.net>
2023-11-22 18:34:21 +0900

    feat: tmp

--- src/containers/ruby-uploader.jsx
+++ src/containers/ruby-uploader.jsx
@@ -14,63 +14,70 @@ class RubyUploader extends React.Component {
     }
 
     uploadProject() {
-	const idToTarget = {};
-	this.props.vm.runtime.targets.forEach(target => {
-	    idToTarget[target.id] = target;
-	});
-	const targets = [idToTarget[this.props.stage.id]];
-	for (const id in this.props.sprites) {
-	    const sprite = this.props.sprites[id];
-	    targets[sprite.order + 1] = idToTarget[id];
-	}
-	const options = {
-	    requires: [],
-	    withSpriteNew: false
-	};
-	if (this.props.rubyCode.modified) {
-	    options.targetsCode = {
-		[this.props.rubyCode.target.id]: this.props.rubyCode.code
-	    };
-	}
-	//master 部分のみ抽出
-	const targets1 = targets.splice(1,1);
-	const master_code = RubyGenerator.targetsToCode(targets1, options);
-	//master部分を削除してslaveのコードを生成
-	//targets.splice(1, 1);
-	const targets2 = targets.splice(1,2);
-	const slave_code = RubyGenerator.targetsToCode(targets2, options);
+        const idToTarget = {};
+        this.props.vm.runtime.targets.forEach((target) => {
+            idToTarget[target.id] = target;
+        });
+        const targets = [idToTarget[this.props.stage.id]];
+        for (const id in this.props.sprites) {
+            const sprite = this.props.sprites[id];
+            targets[sprite.order + 1] = idToTarget[id];
+        }
+        const options = {
+            requires: [],
+            withSpriteNew: false,
+        };
+        if (this.props.rubyCode.modified) {
+            options.targetsCode = {
+                [this.props.rubyCode.target.id]: this.props.rubyCode.code,
+            };
+        }
+        // master 部分のみ抽出
+        const targets1 = targets.splice(1, 1);
+        const master_code = RubyGenerator.targetsToCode(targets1, options);
+        const base64MasterCode = btoa(
+            String.fromCharCode.apply(
+                null,
+                new TextEncoder().encode(master_code)
+            )
+        );
+        // master部分を削除してslaveのコードを生成
+        // targets.splice(1, 1);
+        // const targets2 = targets.splice(1, 2);
+        // const slave_code = RubyGenerator.targetsToCode(targets2, options);
 
-	//確認
-        console.log("master: " + master_code);
-        console.log("slave:  " + slave_code );
+        // 確認
+        console.log(`master: ${base64MasterCode}`);
 
-	//送信
-	var ele = document.createElement('form');
-	ele.action = 'https://www.epi.it.matsue-ct.jp/j1819/convert/upload.php';
-	ele.method = 'post';
-	ele.setAttribute('target', '_blank');
+        // 送信
+        const CompileServerURI = "https://ceres.epi.it.matsue-ct.ac.jp/compile/code";
+        const res = fetch(CompileServerURI, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                code: base64MasterCode,
+            }),
+        }).then(r => {
+            return r.json().then(j => {
+                console.table(j);
+                window.open(`https://ceres.epi.it.matsue-ct.ac.jp/writer?id=${j.id}`, "_blank");
+            })
+        }).catch(r => {
+            console.error(r)
+        })
 
-	var q = document.createElement('textarea');
-	q.value = master_code;
-	q.name = 'master_code';
-
-	var r = document.createElement('textarea');
-	r.value = slave_code;
-	r.name = 'slave_code';
-
-	ele.appendChild(q);
-	ele.appendChild(r);
-	document.body.appendChild(ele);
-
-	ele.submit();
-	ele.remove();
-
-	return;
+        return res;
     }
 
     render() {
         const { children } = this.props;
-        return children(this.props.className, this.downloadProject, this.uploadProject);
+        return children(
+            this.props.className,
+            this.downloadProject,
+            this.uploadProject
+        );
     }
 }
 
